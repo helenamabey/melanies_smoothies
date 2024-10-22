451813c6e7b190340a3af624e74c45763122fb2e
Helena Mabey <124525903+helenamabey@users.noreply.github.com>
2024-10-22 01:43:43 +0000

    Update streamlit_app.py
    hash out fruityvice due to error

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,5 +47,6 @@ if time_to_insert:
     st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#st.text(fruityvice_response)
+#fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
